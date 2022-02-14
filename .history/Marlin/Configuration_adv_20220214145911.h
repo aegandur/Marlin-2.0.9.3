3fa24d363cea2e9a58f3b6a706b4470382690ea2
aegandur <66595099+aegandur@users.noreply.github.com>
2022-02-14 15:00:09 -0600

    TFT settings

--- .history/Marlin/Configuration_adv_20220214145911.h
+++ .history/Marlin/Configuration_adv_20220214145911.h
@@ -935,7 +935,7 @@
   // Define probe X and Y positions for Z1, Z2 [, Z3 [, Z4]]
   // If not defined, probe limits will be used.
   // Override with 'M422 S<index> X<pos> Y<pos>'
-  #define Z_STEPPER_ALIGN_XY { {  50, 108 }, { 170,  108 } }
+  #define Z_STEPPER_ALIGN_XY { {  50, 108 }, { 175,  108 } }
 
   /**
    * Orientation for the automatically-calculated probe positions.
